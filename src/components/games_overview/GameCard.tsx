9f7ac5512b4e01f448bc8ffefb80996c77e22da6
Tomas Polivka <tom.polivka96@gmail.com>
2024-01-02 22:14:29 +0100

    del unused impo

--- src/components/games_overview/GameCard.tsx
+++ src/components/games_overview/GameCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Space, Tag } from "antd";
+import { Button, Card, Tag } from "antd";
 import { useNavigate } from "react-router-dom";
 
 const { Meta } = Card;
